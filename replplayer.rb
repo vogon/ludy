b87060686b814363b78f729e7f49dbf4fbaa7c52
Colin <vogon@icculus.org>
2013-04-08 19:17:28 -0700

    move pry goop into replplayer

--- replplayer.rb
+++ replplayer.rb
@@ -1,8 +1,39 @@
+require 'pry'
+
 class ReplPlayer
 	attr_accessor :proxy
+	attr_accessor :repl
 
 	def initialize(game)
+		self.repl = Pry.new({ quiet: true, prompt: Pry::SIMPLE_PROMPT })
 		self.proxy = Proxy.new(game, self)
+	end
+
+	def run
+		rel(self.repl, self.proxy)
+	end
+
+	def rel(pry, target)
+		target = Pry.binding_for(target)
+
+		pry.repl_prologue(target)
+
+		break_data = nil
+		exception = catch(:raise_up) do
+		    break_data = catch(:breakout) do
+			    loop do
+		    	    pry.re(pry.binding_stack.last)
+		      	end
+		    end
+		    exception = false
+		end
+
+		raise exception if exception
+
+		break_data
+		
+	ensure
+		pry.repl_epilogue(target)
 	end
 
 	def tell(*args)
